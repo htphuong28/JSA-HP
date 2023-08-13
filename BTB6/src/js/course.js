2a293aaf84fb7d5c8810fcbd023fa80bd7d49ff8
htphuong28 <thieuphuong5knt@gmail.com>
2023-08-13 09:38:12 +0700

    fix

--- BTB6/src/js/course.js
+++ BTB6/src/js/course.js
@@ -1,56 +1,56 @@
-let courseList = [
-    {
-        img: "img/stock1.jpg",
-        name: "Basic Course",
-        description: "Description is put here",
-        status: 1,
-        progress: 60,
-        link: "coursedetail.html"
-    },
-    {
-        img: "img/stock1.jpg",
-        name: "Medium Course",
-        description: "Start this after the Basic Course",
-        status: 1,
-        progress: 60,
-    },
-    {
-        img: "img/stock1.jpg",
-        name: "Advance Course",
-        description: "",
-        status: 0,
-        progress: 0,
-    },
-]
+// let courseList = [
+//     {
+//         img: "img/stock1.jpg",
+//         name: "Basic Course",
+//         description: "Description is put here",
+//         status: 1,
+//         progress: 60,
+//         link: "coursedetail.html"
+//     },
+//     {
+//         img: "img/stock1.jpg",
+//         name: "Medium Course",
+//         description: "Start this after the Basic Course",
+//         status: 1,
+//         progress: 60,
+//     },
+//     {
+//         img: "img/stock1.jpg",
+//         name: "Advance Course",
+//         description: "",
+//         status: 0,
+//         progress: 0,
+//     },
+// ]
 
 
-let courseDisplay = document.querySelector(".course-display")
+// let courseDisplay = document.querySelector(".course-display")
 
-for(let i of courseList) {
-    console.log(i.name)
+// for(let i of courseList) {
+//     console.log(i.name)
 
 
-    courseDisplay.innerHTML += `
-    <div class="course">
-        <a href="">
-            <div class="course-image-section">
-                <img src=${i.img} alt="course-image" class="img"> 
-            </div>
+//     courseDisplay.innerHTML += `
+//     <div class="course">
+//         <a href="">
+//             <div class="course-image-section">
+//                 <img src=${i.img} alt="course-image" class="img"> 
+//             </div>
             
-            <div class="course-info">
-                <h2 class="course-name">${i.name}</h2>
-                <p class="course-description">${i.description}</p>
-            </div>
-            <div class="course-individual-info">
-                <a href="${i.link}"><button class="course-action-status">${i.status==1?"Continue":"Start"}</button></a>
+//             <div class="course-info">
+//                 <h2 class="course-name">${i.name}</h2>
+//                 <p class="course-description">${i.description}</p>
+//             </div>
+//             <div class="course-individual-info">
+//                 <a href="${i.link}"><button class="course-action-status">${i.status==1?"Continue":"Start"}</button></a>
                 
-                <progress value="${i.progress}" max="100" class="course-progress"></progress>
-            </div>
+//                 <progress value="${i.progress}" max="100" class="course-progress"></progress>
+//             </div>
         
-        </a>
-    </div>`
+//         </a>
+//     </div>`
     
 
     
 
-}
+// }
--- BTB6/src/js/coursedetail.js
+++ BTB6/src/js/coursedetail.js
@@ -2,6 +2,9 @@ let courseContent = {
     name: "Khóa nhập môn",
     description: "Khóa học đầu tiên giúp bạn làm quen với các kí hiệu cơ bản, tính chất của ngôn ngữ kí hiệu, là nền tảng cho việc học sâu hơn",
     progress: 70,
+    status: 1,
+    img: "img/stock1.jpg",
+    link: "coursedetail.html",
     chapter: [
         {
             name: "chào hỏi cơ bản",
@@ -46,7 +49,69 @@ let courseContent = {
     ]
 }
 
+let courseList = [
+    {
+        img: "img/stock1.jpg",
+        name: "Basic Course",
+        description: "Description is put here",
+        status: 1,
+        progress: 60,
+        link: "coursedetail.html"
+    },
+    {
+        img: "img/stock1.jpg",
+        name: "Medium Course",
+        description: "Start this after the Basic Course",
+        status: 1,
+        progress: 60,
+    },
+    {
+        img: "img/stock1.jpg",
+        name: "Advance Course",
+        description: "",
+        status: 0,
+        progress: 0,
+    },
+]
 
+courseList.unshift(courseContent)
+
+let courseDisplay = document.querySelector(".course-display")
+
+function renderCourseDisplay(placeholder) {
+    if(placeholder) {
+        for(let i of courseList) {
+            console.log(i.name)
+        
+        
+            courseDisplay.innerHTML += `
+            <div class="course">
+                <a href="">
+                    <div class="course-image-section">
+                        <img src=${i.img} alt="course-image" class="img"> 
+                    </div>
+                    
+                    <div class="course-info">
+                        <h2 class="course-name">${i.name}</h2>
+                        <p class="course-description">${i.description}</p>
+                    </div>
+                    <div class="course-individual-info">
+                        <a href="${i.link}"><button class="course-action-status">${i.status==1?"Continue":"Start"}</button></a>
+                        
+                        <progress value="${i.progress}" max="100" class="course-progress"></progress>
+                    </div>
+                
+                </a>
+            </div>`
+            
+        
+            
+        
+        }
+    }
+}
+
+renderCourseDisplay(courseDisplay)
 
 function renderCourseDetail(placeholder) {
     if(placeholder) {
